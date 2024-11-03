e6183569b6c987ca2d918ac144b96659424939b0
sakshamdhakal <152693537+sakshamdhakal@users.noreply.github.com>
2024-11-03 11:11:03 +0545

    list slicing

--- Todo List/main.py
+++ Todo List/main.py
@@ -2,77 +2,93 @@ while True:
     # getting user input
     user_input = input("Type add, show, edit, complete, or exit:").strip() #removes any spaces in input
 
-    match user_input:
-        case 'add' :
-            todo = input("Enter a todo:").strip() 
+   
+    if user_input.startswith("add") :
+        #list slicing
+        todo = user_input[4:] #only takes the input from and after index 4 in user_input 
 
-            if todo:  
-                with open('todos.txt', 'a') as file :
-                    file.write(todo + "\n") # adds a newline so every todo is in a new line
-            else :
-                    print("Input cannot be empty")
-               
-        case 'show':
-            with open('todos.txt','r') as file :
-                todos = file.readlines() # reads everything in file and copies it in todos
+        if todo:  
+            with open('todos.txt', 'a') as file :
+                file.write(todo + "\n") # adds a newline so every todo is in a new line
+        else :
+                print("Input cannot be empty")
+            
+    elif user_input.startswith("show") :
+        with open('todos.txt','r') as file :
+            todos = file.readlines() # reads everything in file and copies it in todos
 
-            if todos: 
+        if todos: 
 
-                # new_todos = [item.strip('\n') for item in todos] list comprehension
+            # new_todos = [item.strip('\n') for item in todos] list comprehension
 
-                for index, item in enumerate(todos) :
-                    row = f"{index + 1}.{item.strip('\n')}" # strip removes unnecessary new line
-                    print(row.title()) # strip can also be used while printing
-            else :
-                    print("No Todos found. Please add one.")
+            for index, item in enumerate(todos) :
+                row = f"{index + 1}.{item.strip('\n')}" # strip removes unnecessary new line
+                print(row.title()) # strip can also be used while printing
+        else :
+                print("No Todos found. Please add one.")
 
-        case 'edit':
-            with open('todos.txt','r') as file :
-                todos = file.readlines()
+    elif user_input.startswith("edit") :
+        with open('todos.txt','r') as file :
+            todos = file.readlines()
 
-            if todos : 
-                number = int(input("Number of the todo to edit:"))
-                
-                if 0 < number <= len(todos) : 
-                    number = number - 1
-                    existing_todo = todos[number]
-                    print(existing_todo.title())
-                    new_todo = input("Enter a new todo:")
-                    if new_todo :
-                        todos[number] = new_todo + "\n"
-                        with open('todos.txt','w') as file : # opens a txt file 
-                            file.writelines(todos)
-                    else :
-                        print("New todo cannot be empty!")
-                else :
-                    print("Enter a correct number.")
-            else :
-                print("No todos to edit:")
-
-        case 'complete':
-            with open('todos.txt','r') as file :
-                todos = file.readlines()
-                if todos:
-                    number = int(input("Number of the todo to complete:"))
-
-                    if 0 < number <= len(todos) : #checks if the number is in the list or 
-                        index = number - 1
-                        remove_todo = todos[index].strip('\n')
-                        todos.pop(index)
-                        
-                        with open('todos.txt','w') as file :
-                            file.writelines(todos)
-                        
-                        message = f"Todo {remove_todo.title()} was removed"
-                        print(message)
+        if todos : 
+            number_str = user_input[5:].strip()
+            # print(number)
+            if number_str :
+                try :
+                    number = int(number_str)
+                    if 0 < number <= len(todos) : 
+                        number = number - 1
+                        existing_todo = todos[number]
+                        print(existing_todo.title())
+                        new_todo = input("Enter a new todo:")
+                        if new_todo :
+                            todos[number] = new_todo + "\n"
+                            with open('todos.txt','w') as file : # opens a txt file 
+                                file.writelines(todos)
+                        else :
+                            print("New todo cannot be empty!")
                     else :
                         print("Enter a correct number.")
-                else :
-                    print("No todos to complete!")
+                    
+                except ValueError:
+                    print("Please input a number")
+                    continue
+            else:
+                print("Input cannot be empty")
+        else :
+            print("No todos to edit:")
 
-        case 'exit':
-            break
-        case everything_else : 
-            print("Wrong Input, Please Try Again!!")
+    elif user_input.startswith("complete") :
+        with open('todos.txt','r') as file :
+            todos = file.readlines()
+            if todos:
+                number_str = user_input[9:].strip()
+                if number_str:
+                    try :
+                        number = int(number_str)
+
+                        if 0 < number <= len(todos) : #checks if the number is in the list or 
+                            index = number - 1
+                            remove_todo = todos[index].strip('\n')
+                            todos.pop(index)
+                            
+                            with open('todos.txt','w') as file :
+                                file.writelines(todos)
+                            
+                            message = f"Todo {remove_todo.title()} was removed"
+                            print(message)
+                        else :
+                            print("Enter a correct number.")
+                    except ValueError :
+                        print("Please Enter a Number!")
+            else :
+                print("No todos to complete!")
+
+    elif 'exit' in user_input :
+        break
+
+    else :
+        print("wrong input!")
 
 print("Bye!")
