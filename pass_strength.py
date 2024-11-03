6c8d24232454fa6d63a64c3e4d54e459afbf6c03
sakshamdhakal <152693537+sakshamdhakal@users.noreply.github.com>
2024-11-03 11:13:34 +0545

    Add files via upload
    Password strength checker using simple python functions.

---
+++ pass_strength.py
@@ -0,0 +1,36 @@
+# length >= 8
+# number atleast 1
+# uppercase at least 1
+
+while True :
+    password = input("Enter your password:")
+    result = {}
+
+
+    if len(password) >= 8 :
+        result["length"] = True
+
+    else :
+        result["length"] = False
+    
+    upper_case = False
+
+    for i in password :
+        if i.isupper() :
+            upper_case = True
+    result["upper_case"] = upper_case
+
+    digits = False
+
+    for i in password :
+        if i.isdigit() :
+            digits = True
+    result["digits"] = digits
+
+    if all(result.values()):
+        print("Strong Password")
+    else :
+        print("Weak Password")
+
+    for key, value in result.items() :
+        print(f"{key}={value}")
