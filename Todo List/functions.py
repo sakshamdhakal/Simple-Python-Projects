ec2fcaa9fce165841f9938a707cef782f08a4cdb
Saksham Dhakal <152693537+sakshamdhakal@users.noreply.github.com>
2024-11-12 19:01:41 +0545

    Update functions.py

--- Todo List/functions.py
+++ Todo List/functions.py
@@ -1,11 +1,19 @@
-def read_todos(filepath="todos.txt") :
+filepath = "todos.txt"
+
+def read_todos(filepath) :
     """ Read a text file and returns the list of todo items."""
     with open(filepath,'r') as file_local :
         todos_local = file_local.readlines() # reads everything in file and copies it in todos
     return todos_local
 
 
-def write_todos(todos_arg, filepath="todos.txt") :
+def write_todos(todos_arg, filepath) :
     """ Writes the todo items list in the text file."""
     with open(filepath,'w') as file : # opens a txt file 
        file.writelines(todos_arg)
+
+import time
+
+if __name__ == "__main__" :
+    now = time.strftime("%b %d %H:%M:%S %Y")
+    print("It is", now)
